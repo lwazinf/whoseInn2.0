a77455f0fe6da08201ea5ade56dd638d1f456675
LwaziNF <lwazinf@gmail.com>
2023-01-20 02:14:17 +0200

    ui :: pre yarn package, react-google-autocomplete

--- components/Create_.tsx
+++ components/Create_.tsx
@@ -1,5 +1,13 @@
 import { useRecoilState } from "recoil";
-import { ThisState } from "./atoms/atoms";
+import {
+  ThisState,
+  AccrState,
+  ImageState,
+  LocationState,
+  ServicesState,
+  PriceState,
+  StudentsState,
+} from "./atoms/atoms";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowRight,
@@ -19,11 +27,18 @@ interface Create_Props {}
 
 const Create_ = ({}: Create_Props) => {
   const acc = "Your current level of accreditation..";
-  const images = "A cover image for your accommodation..";
+  const images = "Choose a cover image for your accommodation..";
   const price = "Your price per room..";
   const map = "Let's find your location..";
   const students = "How many rooms are available??";
   const services = "Which services do you provide?";
+
+  const [accr_, setAccr_] = useRecoilState(AccrState);
+  const [image_, setImage_] = useRecoilState(ImageState);
+  const [location_, setLocation_] = useRecoilState(LocationState);
+  const [services_, setServices_] = useRecoilState(ServicesState);
+  const [price_, setPrice_] = useRecoilState(PriceState);
+  const [students_, setStudents_] = useRecoilState(StudentsState);
 
   const [showThis_, setShowThis_] = useRecoilState(ThisState);
   const [option_, setOption_] = useState(0);
@@ -78,6 +93,20 @@ const Create_ = ({}: Create_Props) => {
       icon: faCoins,
     },
   ];
+
+  const onMutate = (e: any) => {
+    if (e.target.files) {
+      // setModal(true)
+      const tempData_: any = [
+        e.target.files[0],
+        URL.createObjectURL(e.target.files[0]),
+      ];
+      setImage_(tempData_);
+    } else {
+      console.log("No Images Selected!");
+    }
+  };
+
   return (
     <div
       className={`w-full min-h-screen fixed top-0 left-0 flex flex-col justify-center items-center bg-black/40 backdrop-blur-sm transition-all duration-200 ${
@@ -107,6 +136,14 @@ const Create_ = ({}: Create_Props) => {
                     setOption_(option_ + 1);
                   } else {
                     // Confirmation Step..
+                    console.log({
+                      accr: accr_,
+                      location: location_,
+                      price: price_,
+                      students: students_,
+                      services: services_,
+                      image: image_,
+                    });
                   }
                 }}
               >
@@ -119,14 +156,18 @@ const Create_ = ({}: Create_Props) => {
           return (
             <div
               className={`w-[400px] h-[450px] absolute right-[50px] flex flex-col justify-end items-center pl-6 pb-5 transition-all duration-200 ${
-                obj.data == mockData_[option_].data
+                obj.data == mockData_[option_].data && showThis_ == "create"
                   ? "opacity-100 pointer-events-auto"
                   : "opacity-0 pointer-events-none"
               }`}
               key={obj.data}
             >
               <p
-                className={`mb-8 text-[20px] font-black text-center w-full h-full flex flex-col justify-center items-center text-black/50`}
+                className={`mb-8 text-[20px] font-black text-center w-[400px] h-[450px] flex flex-col justify-center items-center text-black/50 pt-7 ${
+                  obj.data == mockData_[option_].data && showThis_ == "create"
+                    ? "opacity-100 pointer-events-auto"
+                    : "opacity-0 pointer-events-none"
+                }`}
               >
                 {obj.data}
               </p>
@@ -135,6 +176,13 @@ const Create_ = ({}: Create_Props) => {
                   type={"text"}
                   placeholder={`Your answer here..`}
                   className={`w-full h-[30px] bg-black/10 rounded-[2px] shadow-sm pl-2`}
+                  onChange={(e) => {
+                    if (obj.data == students) {
+                      setStudents_(e.target.value);
+                    } else {
+                      setPrice_(e.target.value);
+                    }
+                  }}
                 />
               ) : obj.optionType == "select" ? (
                 <div
@@ -143,21 +191,73 @@ const Create_ = ({}: Create_Props) => {
                   {obj.options.map((obj_) => {
                     return (
                       <div
-                        className={`w-[60px] h-[30px] bg-black/10 rounded-[2px] shadow-sm cursor-pointer flex flex-row justify-center items-center mx-1`}
+                        className={`w-[100px] h-[30px] ${
+                          accr_ == obj_ ? "bg-black/70" : "bg-black/40"
+                        } hover:bg-black/50 transition-all duration-200 rounded-[4px] shadow-sm cursor-pointer flex flex-row justify-center items-center mx-1 text-white text-[15px] font-black`}
+                        onClick={() => {
+                          setAccr_(obj_);
+                        }}
                       >
-                        {
-                          obj_
-                        }
+                        {obj_}
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : obj.optionType == "images" ? (
+                <div
+                  className={`w-[100px] h-[30px] flex flex-col justify-center items-center`}
+                >
+                  <input
+                    type={"file"}
+                    id="images"
+                    max="6"
+                    accept=".jpg,.png,.jpeg"
+                    onChange={onMutate}
+                    multiple
+                    required
+                    hidden
+                    className={`w-full h-[30px] bg-black/10 rounded-[2px] shadow-sm pl-2`}
+                  />
+                  <label htmlFor="images">
+                    <div
+                      className={`w-[120px] h-[30px] flex flex-col justify-center items-center bg-black/40 hover:bg-black/50 transition-all duration-200 rounded-[4px] shadow-sm cursor-pointer mx-1 text-white text-[15px] font-black`}
+                    >
+                      Select
+                    </div>
+                  </label>
+                </div>
+              ) : obj.optionType == "toggle" ? (
+                <div
+                  className={`flex flex-row justify-center items-center w-full`}
+                >
+                  {obj.options.map((obj_) => {
+                    return (
+                      <div
+                        className={`${
+                          services_.includes(obj_)
+                            ? "bg-blue-500/80 hover:bg-red-500/40 text-white"
+                            : "hover:bg-blue-500/40 bg-white/50 hover:text-white text-blue-300"
+                        } flex w-[81px] h-[31px] rounded-md flex-row items-center justify-center text-center transition-all duration-400 cursor-pointer m-1`}
+                        onClick={() => {
+                          console.log(services_)
+                          if(services_.includes(obj_)){
+                          }else{
+                            setServices_([...services_, obj_])
+                          }
+                        }}
+                        key={obj_}
+                      >
+                        <div
+                          className={`flex w-[80px] h-[30px] rounded-md flex-row items-center justify-center text-center transition-all duration-400`}
+                        >
+                          <p className={`font-medium text-[14px]`}>{obj_}</p>
+                        </div>
                       </div>
                     );
                   })}
                 </div>
               ) : (
-                <input
-                  type={"text"}
-                  placeholder={`Your answer here..`}
-                  className={`w-full h-[30px] bg-black/10 rounded-[2px] shadow-sm pl-2`}
-                />
+                <></>
               )}
             </div>
           );
@@ -165,6 +265,13 @@ const Create_ = ({}: Create_Props) => {
         <div
           className={`min-h-[20px] min-w-[20px] flex flex-row justify-center items-center cursor-pointer text-black/40 hover:text-black/60 transition-all duration-200 absolute top-0 right-0 m-4`}
           onClick={() => {
+            setAccr_("");
+            setImage_("");
+            setPrice_("");
+            setServices_("");
+            setStudents_("");
+            setLocation_("");
+
             setShowThis_("");
             setOption_(0);
           }}
--- components/atoms/atoms.ts
+++ components/atoms/atoms.ts
@@ -4,3 +4,33 @@ export const ThisState = atom({
     key: 'ThisState',
     default: '',
   });
+
+  export const ServicesState = atom<any>({
+    key: 'ServicesState',
+    default: [],
+  });
+
+  export const PriceState = atom({
+    key: 'PriceState',
+    default: '',
+  });
+
+  export const StudentsState = atom({
+    key: 'StudentsState',
+    default: '',
+  });
+
+  export const LocationState = atom({
+    key: 'LocationState',
+    default: '',
+  });
+
+  export const ImageState = atom({
+    key: 'ImageState',
+    default: '',
+  });
+
+  export const AccrState = atom({
+    key: 'AccrState',
+    default: '',
+  });
