87dea3df32b5a23b2251dce0aea34b5b28cfc69a
LwaziNF <lwazinf@gmail.com>
2023-01-23 08:49:46 +0200

    deployment fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
   useEffect(() => {
     const y_ = async () => {
       const x_ = await getLocations();
+      // @ts-ignore
       setCache_(x_);
     };
     y_();
@@ -21,23 +22,27 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3x0RxMZbl6LRapAhKlegw6Xmp9rupr0g&libraries=places"></script>
       <main className="flex w-full h-full flex-col items-center justify-center text-center">
-        <div className={`m-auto grid grid-cols-3 gap-4 flex flex-col items-center justify-center`}>
-          {
-          cache_?.map((obj) => {
+        <div
+          className={`m-auto grid grid-cols-3 gap-4 flex flex-col items-center justify-center`}
+        >
+          {cache_?.map((obj) => {
             return (
               <div
                 className={`w-[350px] h-[200px] bg-white rounded-lg shadow-sm cursor-pointer overflow-hidden relative opacity-80 hover:opacity-100 transition-all duration-200 p-1`}
                 onClick={() => {
                   setShowThis_("accom");
-                  setFocus_(obj)
+                  setFocus_(obj);
                 }}
                 key={obj}
               >
-                <img className={`w-full h-full object-cover rounded-lg`} src={obj?.image}/>
+                {/* @ts-ignore */}
+                <img
+                  className={`w-full h-full object-cover rounded-lg`}
+                  src={obj?.image}
+                />
               </div>
             );
-          })
-          }
+          })}
         </div>
       </main>
     </div>
