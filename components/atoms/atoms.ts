767e3ed795ad3787bb77ca93aa43e332183c9acf
LwaziNF <lwazinf@gmail.com>
2023-01-30 11:02:53 +0200

    vercel fix

--- components/atoms/atoms.ts
+++ components/atoms/atoms.ts
@@ -1,64 +1,64 @@
-import { atom } from "recoil";
+import { atom } from "recoil"
 
 export const ThisState = atom({
     key: 'ThisState',
     default: 'auth',
-  });
+  })
 
   export const ServicesState = atom<any>({
     key: 'ServicesState',
     default: [],
-  });
+  })
 
   export const PriceState = atom({
     key: 'PriceState',
     default: 0,
-  });
+  })
 
   export const StudentsState = atom({
     key: 'StudentsState',
     default: 0,
-  });
+  })
 
   export const LocationState = atom<any>({
     key: 'LocationState',
     default: {},
-  });
+  })
 
   export const ImageState = atom<any>({
     key: 'ImageState',
     default: [],
-  });
+  })
 
   export const AccrState = atom({
     key: 'AccrState',
     default: '',
-  });
+  })
 
   export const NotifState = atom({
     key: 'NotifState',
     default: '',
-  });
+  })
 
   export const CacheState = atom({
     key: 'CacheState',
     default: [],
-  });
+  })
 
   export const FocusState = atom({
     key: 'FocusState',
     default: {},
-  });
+  })
 
   export const MapState = atom({
     key: 'MapState',
     default: false,
-  });
+  })
 
   export const IsSignInState = atom({
     key: 'IsSignInState',
     default: false,
-  });
+  })
 
   export const HoverState = atom({
     key: 'HoverState',
--- firebase.ts
+++ firebase.ts
@@ -22,7 +22,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let analytics;
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
 const auth = getAuth(app);
 const db = getFirestore(app);
 const store = getStorage(app);
